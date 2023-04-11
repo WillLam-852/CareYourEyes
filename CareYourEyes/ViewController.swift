e6ab597d549e322943345ca5b27727b560b6b04b
Lam Wun Yin, Will <willyin@connect.hku.hk>
2023-04-11 15:01:29 +0800

    Initial Commit

---
+++ CareYourEyes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CareYourEyes
+//
+//  Created by Lam Wun Yin on 11/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
